ba10b6590e821c2a35328ef36063f5fb4253cf95
muhammedhaleel <muhammedhaleel700@gmail.com>
2026-06-17 22:17:38 +0530

    updated

--- snapshire/snapshire/settings.py
+++ snapshire/snapshire/settings.py
@@ -79,12 +79,18 @@ WSGI_APPLICATION = 'snapshire.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'snapshire_db',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
