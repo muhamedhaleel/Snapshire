72974602798b885c1e560d118304b382da67dcd4
muhammedhaleel <muhammedhaleel700@gmail.com>
2026-06-18 13:11:24 +0530

    add user table

--- snapshire/user/models.py
+++ snapshire/user/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'User Profile'
+        verbose_name_plural = 'User Profiles'
+
+    def __str__(self):
+        return self.user.username
