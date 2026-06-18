81ca69b4580e06701698fd5bad7e3c3254660cd3
muhammedhaleel <muhammedhaleel700@gmail.com>
2026-06-18 13:56:38 +0530

    chage urls

--- snapshire/user/urls.py
+++ snapshire/user/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('signup/', views.signup, name='signup'),
