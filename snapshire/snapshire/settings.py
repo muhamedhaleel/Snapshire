1092e6a87a00aa2c7432ccd46b22412a0b6d8460
muhammedhaleel <muhammedhaleel700@gmail.com>
2026-06-19 11:53:19 +0530

    addes

--- snapshire/snapshire/settings.py
+++ snapshire/snapshire/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user'
+    'user',
+    'photographer',
 ]
 
 MIDDLEWARE = [
