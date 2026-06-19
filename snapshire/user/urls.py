e4dd46145ee34d0df578c11a6123c4840a89ab84
muhammedhaleel <muhammedhaleel700@gmail.com>
2026-06-19 23:11:34 +0530

    update index redirect page

--- snapshire/user/urls.py
+++ snapshire/user/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('home/', views.home, name='home'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('login/', views.user_login, name='login'),
