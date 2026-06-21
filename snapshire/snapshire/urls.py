b8049cd05d07b81dfa1e36fab0d744b6060a47f6
muhammedhaleel <muhammedhaleel700@gmail.com>
2026-06-21 15:44:41 +0530

    changes

--- snapshire/snapshire/urls.py
+++ snapshire/snapshire/urls.py
@@ -1,6 +1,8 @@
 """
 URL configuration for snapshire project.
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -10,3 +12,6 @@ urlpatterns = [
     path('', include('user.urls')),
     path('photographer/', include('photographer.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
