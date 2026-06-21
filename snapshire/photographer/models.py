9c11de3e7cd4a866075282e6bb5b9f2a74c01abf
muhammedhaleel <muhammedhaleel700@gmail.com>
2026-06-21 16:42:55 +0530

    ss

--- snapshire/photographer/models.py
+++ snapshire/photographer/models.py
@@ -7,6 +7,11 @@ def photographer_profile_image_path(instance, filename):
     return f'photographer_profiles/{instance.user_id}/profile.{extension}'
 
 
+def portfolio_file_path(instance, filename):
+    safe_name = filename.replace(' ', '_')
+    return f'photographer_portfolios/{instance.profile.user_id}/{safe_name}'
+
+
 class PhotographerProfile(models.Model):
     PLAN_FREE = 'free'
     PLAN_GOLD = 'gold'
@@ -69,3 +74,30 @@ class PortfolioLink(models.Model):
 
     def __str__(self):
         return self.url
+
+
+class PortfolioFile(models.Model):
+    profile = models.ForeignKey(
+        PhotographerProfile,
+        on_delete=models.CASCADE,
+        related_name='portfolio_files',
+    )
+    file = models.FileField(upload_to=portfolio_file_path)
+    original_name = models.CharField(max_length=255, blank=True, default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return self.original_name or self.file.name
+
+    def save(self, *args, **kwargs):
+        if self.file and not self.original_name:
+            self.original_name = self.file.name
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        if self.file:
+            self.file.delete(save=False)
+        super().delete(*args, **kwargs)
