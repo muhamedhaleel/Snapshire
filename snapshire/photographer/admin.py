9abea56f368cdf3b11d76ebec1890992e90a8921
muhammedhaleel <muhammedhaleel700@gmail.com>
2026-06-21 16:42:39 +0530

    asses

--- snapshire/photographer/admin.py
+++ snapshire/photographer/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import PhotographerProfile, PortfolioLink
+from .models import PhotographerProfile, PortfolioFile, PortfolioLink
 
 
 class PortfolioLinkInline(admin.TabularInline):
@@ -8,14 +8,26 @@ class PortfolioLinkInline(admin.TabularInline):
     extra = 0
 
 
+class PortfolioFileInline(admin.TabularInline):
+    model = PortfolioFile
+    extra = 0
+    readonly_fields = ('original_name', 'created_at')
+
+
 @admin.register(PhotographerProfile)
 class PhotographerProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'specialty', 'location', 'plan_mode', 'phone', 'created_at')
     search_fields = ('user__username', 'user__email', 'specialty', 'location')
-    inlines = [PortfolioLinkInline]
+    inlines = [PortfolioLinkInline, PortfolioFileInline]
 
 
 @admin.register(PortfolioLink)
 class PortfolioLinkAdmin(admin.ModelAdmin):
     list_display = ('profile', 'url', 'created_at')
     search_fields = ('url', 'profile__user__username')
+
+
+@admin.register(PortfolioFile)
+class PortfolioFileAdmin(admin.ModelAdmin):
+    list_display = ('profile', 'original_name', 'created_at')
+    search_fields = ('original_name', 'profile__user__username')
