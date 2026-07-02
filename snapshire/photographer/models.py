c27e7a93a828e3c1ba5c263ce539d6864ce6f0c4
muhammedhaleel <muhammedhaleel700@gmail.com>
2026-07-03 00:49:14 +0530

    change to drf

--- snapshire/photographer/models.py
+++ snapshire/photographer/models.py
@@ -34,6 +34,8 @@ class PhotographerProfile(models.Model):
     specialty = models.CharField(max_length=100, blank=True, default='')
     location = models.CharField(max_length=150, blank=True, default='')
     profile_image = models.ImageField(upload_to=photographer_profile_image_path, blank=True, null=True)
+    portfolio_link = models.URLField(blank=True,default="")
+    portfolio_pdf = models.FileField(upload_to="photographer_portfolios/",blank=True,null=True)
     plan_mode = models.CharField(max_length=20, choices=PLAN_CHOICES, blank=True, default='')
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -44,60 +46,8 @@ class PhotographerProfile(models.Model):
     def __str__(self):
         return self.user.username
 
-    @property
-    def is_profile_complete(self):
-        return all([
-            self.phone.strip(),
-            self.bio.strip(),
-            self.specialty.strip(),
-            self.location.strip(),
-        ])
-
-    @property
-    def plan_mode_label(self):
-        if not self.plan_mode:
-            return 'No plan selected'
-        return self.get_plan_mode_display()
+    
 
 
-class PortfolioLink(models.Model):
-    profile = models.ForeignKey(
-        PhotographerProfile,
-        on_delete=models.CASCADE,
-        related_name='portfolio_links',
-    )
-    url = models.URLField(max_length=500)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ['created_at']
-
-    def __str__(self):
-        return self.url
 
 
-class PortfolioFile(models.Model):
-    profile = models.ForeignKey(
-        PhotographerProfile,
-        on_delete=models.CASCADE,
-        related_name='portfolio_files',
-    )
-    file = models.FileField(upload_to=portfolio_file_path)
-    original_name = models.CharField(max_length=255, blank=True, default='')
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ['created_at']
-
-    def __str__(self):
-        return self.original_name or self.file.name
-
-    def save(self, *args, **kwargs):
-        if self.file and not self.original_name:
-            self.original_name = self.file.name
-        super().save(*args, **kwargs)
-
-    def delete(self, *args, **kwargs):
-        if self.file:
-            self.file.delete(save=False)
-        super().delete(*args, **kwargs)
