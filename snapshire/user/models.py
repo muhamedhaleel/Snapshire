3c37e0bca5c0e6274c8e0f8f61b6cca5f6b7a093
muhammedhaleel <muhammedhaleel700@gmail.com>
2026-07-03 00:51:03 +0530

    change to drf

--- snapshire/user/models.py
+++ snapshire/user/models.py
@@ -6,6 +6,17 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     phone = models.CharField(max_length=20, blank=True, default='')
     bio = models.TextField(blank=True, default='')
+    location = models.CharField(max_length=100, blank=True, default='')
+    gender = models.CharField(
+        max_length=10,
+        choices=[
+            ('Male', 'Male'),
+            ('Female', 'Female'),
+            ('Other', 'Other'),
+        ],
+        blank=True,
+        default=''
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
